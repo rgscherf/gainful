2992dff9a962ab8285859cd1e3a6fc3bbd692d6b
rgscherf <rgscherf@gmail.com>
2015-07-16 16:56:11 -0400

    added size log

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template
 # from flask.ext.sqlalchemy import SQLAlchemy
 from src import parse
+import sys
 
 app = Flask(__name__)
 
 @app.route("/")
 def template_test():
     data = parse.build_parse_list()
-    return render_template('template_test.html', data=data)
+    size = str(sys.getsizeof(data))
+    return render_template('template_test.html', data=data, size=size)
 
 if __name__ == '__main__':
     app.run(debug=True)
